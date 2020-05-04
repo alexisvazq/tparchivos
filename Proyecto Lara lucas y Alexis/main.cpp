963c449adff0854b048290071ced7e419ed3845e
Alexis V <vazquez.alexis.e@gmail.com>
2020-05-03 21:18:23 -0300

    Update main.cpp

--- Proyecto Lara lucas y Alexis/main.cpp
+++ Proyecto Lara lucas y Alexis/main.cpp
@@ -16,7 +16,7 @@ int main(){
         cout<<"-------MENU PRINCIPAL--------"<<endl;
         cout<<"-----------------------------"<<endl;
         cout<<"     1)  PLATOS"<<endl;
-        cout<<"     2)  CLIENTES"<<endl,
+        cout<<"     2)  CLIENTES"<<endl;
         cout<<"     3)  PEDIDOS"<<endl;
         cout<<"     4)  REPORTES"<<endl;
         cout<<"     5)  CONFIGURACION"<<endl;
@@ -34,7 +34,7 @@ int main(){
                     cout<<"-------MENU PLATOS-----------"<<endl;
                     cout<<"-----------------------------"<<endl;
                     cout<<"     1) NUEVO PLATO"<<endl;
-                    cout<<"     2) MODIFICAR PLATO"<<endl,
+                    cout<<"     2) MODIFICAR PLATO"<<endl;
                     cout<<"     3) LISTAR PLATO POR ID"<<endl;
                     cout<<"     4) PLATOS POR RESTAURANTE"<<endl;
                     cout<<"     5) LISTAR TODOS LOS PLATOS"<<endl;
