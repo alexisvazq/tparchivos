9d0ebd024521b03e3815d566992bd5a8414953b7
Alexis V <vazquez.alexis.e@gmail.com>
2020-05-22 00:07:48 -0300

    Backup

--- Proyecto Lara lucas y Alexis/configuracion.h
+++ Proyecto Lara lucas y Alexis/configuracion.h
@@ -1,74 +1,101 @@
 #ifndef CONFIGURACION_H_INCLUDED
 #define CONFIGURACION_H_INCLUDED
 
-void copia_seguridad(platos *regplatos,clientes *regclientes,pedidos *regpedidos)
-{
-  FILE *pplatos,*pcoplatos,*pclientes,*pcoclientes,*ppedidos,*pcopedidos;
+bool doBackupPlatos();
+bool doBackupClientes();
+bool doBackupPedidos();
 
-  pplatos = fopen(PATH_PLATOS,"rb");
-  if(pplatos == NULL){
-        system("cls");
-      cout << "EL ARCHIVO PLATOS NO PUDO SER ABIERTO " << endl;
-      anykey();
-    return;
-  }
-  pclientes = fopen(PATH_CLIENTES,"rb");
-  if(pclientes == NULL){
-    system("cls");
-    cout << "  EL ARCHIVO CLIENTES NO SE PUDO ABRIR " << endl;
-    anykey();
-    return;
-  }
-  ppedidos = fopen(PATH_PEDIDOS,"rb");
-  if(ppedidos == NULL){
-    system("cls");
-    cout << " EL ARCHIVO PEDIDOS NO SE PUDO ABRIR " << endl;
-    anykey();
-    return;
-  }
-  pcoplatos = fopen("platos.bkp","ab");
-  if(pcoplatos == NULL){
-        system("cls");
-    cout << " NO SE PUDO ABRIR EL ARCHIVO COPIA PLATOS " << endl;
-    anykey();
-    return;
-  }
-  pcoclientes = fopen("clientes.bkp","ab");
-  if(pcoclientes == NULL){
-    system("cls");
-    cout << " NO SE PUDO ABRIR EL ARCHIVO COPIA CLIENTES " << endl;
-    anykey();
-    return;
-  }
-  pcopedidos = fopen("pedidos.bkp","ab");
-  if(pcoclientes == NULL){
-    system("cls");
-    cout << " NO SE PUDO ABRIR EL ARCHIVO COPIA PEDIDOS " << endl;
-    anykey();
-    return;
-  }
-  while(fread(&regplatos,sizeof(platos),1,pplatos) == 1){
-    fwrite(&regplatos,sizeof(platos),1,pcoplatos);
-  }
-  while(fread(&regclientes,sizeof(clientes),1,pclientes) == 1){
-    fwrite(&regclientes,sizeof(clientes),1,pcoclientes);
-  }
-  while(fread(&regpedidos,sizeof(pedidos),1,ppedidos) == 1){
-    fwrite(&regpedidos,sizeof(pedidos),1,pcopedidos);
-  }
-  fclose(pclientes);
-  fclose(pcoclientes);
-  fclose(ppedidos);
-  fclose(pcopedidos);
-  fclose(pplatos);
-  fclose(pcoplatos);
-  system("cls");
-  cout << "--------COPIA DE SEGURIDAD REALIZADA--------" << endl;
-  anykey();
+void copia_seguridad(){
+  bool isBackupPlatos = doBackupPlatos();
+  bool isBackupClientes = doBackupClientes();
+  bool isBackupPedidos = doBackupPedidos();
+
+	if( isBackupPlatos && isBackupClientes && isBackupPedidos ){
+		cout<<"Backup exitoso!"<<endl;
+	}else{
+		cout<<"No se pudo crear el backup"<<endl;
+	}
 }
 
+bool doBackupPlatos(){
 
-void restaurar_seguridad(platos *regplatos,clientes *regclientes,pedidos *regpedidos)
+	bool isBackupDone;
+  int cantPlatos = contar_platos();
+
+  platos *platosB = (platos *) malloc(cantPlatos*sizeof(platos));
+  if(platosB==NULL){
+    cout<<"Error de asignación de memoria"<<endl;
+    return false;
+  }
+	
+  FILE *p;
+
+	p = fopen("datos/platos.dat", "rb");
+	int platosLeidos = fread(&platosB[0], sizeof(platos), cantPlatos, p);
+	fclose(p);
+
+	p = fopen("datos/platos.bkp", "wb");
+	int platosEscritos = isBackupDone = fwrite(&platosB[0], sizeof(platos), cantPlatos, p);
+	fclose(p);
+
+	free(platosB);
+
+	return isBackupDone;
+}
+
+bool doBackupClientes(){
+
+	bool isBackupDone;
+  int cantClientes = contar_clientes();
+
+  clientes *clientesB = (clientes *) malloc(cantClientes*sizeof(clientes));
+  if(clientesB==NULL){
+    cout<<"Error de asignación de memoria"<<endl;
+    return false;
+  }
+	
+  FILE *p;
+
+	p = fopen("datos/clientes.dat", "rb");
+	int clientesLeidos = fread(&clientesB[0], sizeof(clientes), cantClientes, p);
+	fclose(p);
+
+	p = fopen("datos/clientes.bkp", "wb");
+	int clientesEscritos = isBackupDone = fwrite(&clientesB[0], sizeof(clientes), cantClientes, p);
+	fclose(p);
+
+	free(clientesB);
+
+	return isBackupDone;
+}
+
+bool doBackupPedidos(){
+
+	bool isBackupDone;
+  int cantPedidos = contar_pedidos();
+
+  pedidos *pedidosB = (pedidos *) malloc(cantPedidos*sizeof(pedidos));
+  if(pedidosB==NULL){
+    cout<<"Error de asignación de memoria"<<endl;
+    return false;
+  }
+	
+  FILE *p;
+
+	p = fopen("datos/pedidos.dat", "rb");
+	int pedidosLeidos = fread(&pedidosB[0], sizeof(pedidos), cantPedidos, p);
+	fclose(p);
+
+	p = fopen("datos/pedidos.bkp", "wb");
+	int pedidosEscritos = isBackupDone = fwrite(&pedidosB[0], sizeof(pedidos), cantPedidos, p);
+	fclose(p);
+
+	free(pedidosB);
+
+	return isBackupDone;
+}
+
+/*void restaurar_seguridad(platos *regplatos,clientes *regclientes,pedidos *regpedidos)
 {
     FILE *pplatos,*pcoplatos,*pclientes,*pcoclientes,*ppedidos,*pcopedidos;
     pcoplatos= fopen("platos.bkp","rb");
@@ -131,6 +158,6 @@ void restaurar_seguridad(platos *regplatos,clientes *regclientes,pedidos *regped
   system("cls");
   cout << "--------COPIA DE SEGURIDAD RESTAURADA-------- " << endl;
   anykey();
-}
+}*/
 
 #endif // CONFIGURACION_H_INCLUDED
